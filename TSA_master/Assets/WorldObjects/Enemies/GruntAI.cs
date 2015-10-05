95eb01186e692a55e1f2e8d97ba96b153ca02888
ConnorPoulton <poulton.cw@gmail.com>
2015-10-05 17:35:34 -0400

    initial set-up
    Let's see if this works...

---
+++ TSA_master/Assets/WorldObjects/Enemies/GruntAI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//Script written by Connor Poulton
+public class GruntAI : MonoBehaviour {
+
+   public int[] nodes; //set value in the Unity editor
+   public List<Transform> nodeTransforms;
+   public NavMeshAgent agent;
+	
+	void Start ()
+    {
+
+        //NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        //TODO develop a way to retrieve the Nav Mesh componenet for the current level
+        if (nodes.Length == 0) //if no nodes are attached to the Grunt, set the current position as a node so it can pathfind back to its starting position
+        {
+            nodeTransforms.Add(this.transform);
+        }else {
+
+            foreach (int node in nodes)
+            {
+                Debug.Log(node.ToString());
+                string nodeName = "node" + nodes[node].ToString();
+               
+                GameObject currentNode = GameObject.Find(nodeName);
+                try
+                { nodeTransforms.Add(currentNode.transform); }
+                catch
+                { Debug.Log("You have mislabled node" + nodes[node].ToString() + " in enemy " + this.name);  break; }
+
+            }
+
+        }
+
+	}
+
+
+    void Update()
+    {
+        
+    }
+	
+	
+}
