9564830c4e80e73ea03c2a4c3cae7b2137f0da94
ConnorPoulton <poulton.cw@gmail.com>
2015-10-10 18:58:41 -0400

    Added PathToNextNode state
    Capitalized Nodes and NodeTransforms to keep syntax consistent (lower
    case show a variable that is an instance of a user-defined type)

--- TSA_master/Assets/WorldObjects/Enemies/GruntAI.cs
+++ TSA_master/Assets/WorldObjects/Enemies/GruntAI.cs
@@ -5,14 +5,21 @@ using System.Collections.Generic;
 //Script written by Connor Poulton
 public class GruntAI : MonoBehaviour {
     //navigation system values
-   public int[] nodes; //set value in the Unity editor
-   public List<Transform> nodeTransforms;
+   public int[] Nodes; //set value in the Unity editor
+   public List<Transform> NodeTransforms;
    public NavMeshAgent agent;
+   private int CurrentNode = 0;
+   private Transform CurrentNodeTransform;
+   private int NodeTransformsMax; //stores last element in list, used to loop back to start of patrol
 
     //state machine variables
-    private enum States {PathToNextNode, WaitAtNode, InvestigateNoise, FoundPlayer};
+    private enum States {PathToNextNode, WaitAtNode, InvestigateNoise, ReturnToPatrol, FoundPlayer};
     private States currentstate;
-    private States laststate;
+    private States laststate = 0;
+
+
+    //----------------initialization---------------------------------------
+
 
 	void Start ()
     {
@@ -24,28 +31,60 @@ public class GruntAI : MonoBehaviour {
         
         //TODO develop a way to associate WaitAtNode instructions with nodes (time to wait, direction to turn)
 
-        if (nodes.Length == 0) //if no nodes are attached to the Grunt, set the current position as a node so it can pathfind back to its starting position
+        if (Nodes.Length == 0) //if no nodes are attached to the Grunt, set the current position as a node so it can pathfind back to its starting position
         {
-            nodeTransforms.Add(this.transform);
+            NodeTransforms.Add(this.transform);
         }else {
 
-            foreach (int node in nodes)
+            foreach (int node in Nodes)
             {
                 Debug.Log(node.ToString());
-                string nodeName = "node" + nodes[node].ToString();
+                string nodeName = "node" + Nodes[node].ToString();
                
                 GameObject currentNode = GameObject.Find(nodeName);
                 try
-                { nodeTransforms.Add(currentNode.transform); }
+                { NodeTransforms.Add(currentNode.transform); }
                 catch
-                { Debug.Log("You have mislabled node" + nodes[node].ToString() + " in enemy " + this.name);  break; }
+                { Debug.Log("You have mislabled node" + Nodes[node].ToString() + " in enemy " + this.name);  break; }
 
             }
 
         }
 
-        this.transform.position = nodeTransforms[0].position;
+        NodeTransformsMax = NodeTransforms.Count - 1;
+        this.transform.position = NodeTransforms[0].position;
 	}
+
+
+    //----------------------AI states------------------------------------------------------
+
+    private IEnumerator PathToNextNode()
+    {
+        CurrentNodeTransform = NodeTransforms[CurrentNode];
+        while (currentstate == States.PathToNextNode)
+        {
+            agent.destination = NodeTransforms[CurrentNode].position;
+
+            if (this.transform.position == NodeTransforms[CurrentNode].position)
+            {
+                if(CurrentNode == NodeTransformsMax)
+                {
+                    CurrentNode = 0;
+                }
+                else{
+                    CurrentNode += 1;
+                }
+
+                States nextstate = States.WaitAtNode; NewState(nextstate);
+            }
+            yield return null;
+        }
+        NextState();
+    }
+
+
+    //---------------state machine functions-----------------------------------------
+
 
     //call in the termination statement of an IEnumerator coroutine
     void NextState()
