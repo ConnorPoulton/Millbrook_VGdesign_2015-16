a80b229bfd7fb4a76a189a3374a24deec70b6ae5
ConnorPoulton <poulton.cw@gmail.com>
2015-11-20 18:47:41 -0500

    praise the good lord, enemy pathing finally done
    fixed GetYangleTowrds function, general bug fixes

--- TSA_master/Assets/WorldObjects/Enemies/Grunt/GruntAI.cs
+++ TSA_master/Assets/WorldObjects/Enemies/Grunt/GruntAI.cs
@@ -9,21 +9,23 @@ using System.Collections.Generic;
 public class Node
 {
     public Transform target;
-    public int degreeTurn;
+    public float degreeTurn;
     public int waitTime;
 }
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class GruntAI : MonoBehaviour {
+public class GruntAI : MonoBehaviour
+{
 
     //navigation system values
     public List<Node> nodes; //set value in the Unity editor
-	NavMeshAgent agent;
+    NavMeshAgent agent;
     int CurrentNode = 0; //since 1 is added each iteration, set to -1 to start at zero
     int MaxNode; //stores last element in list, used to loop back to start of patrol
-    
+    float timeRotating; //used to store the exact time passed while exectuing the rotateGrunt coroutine, keeps timing consistent
+
     //state machine variables
-    enum States {PathToNextNode, WaitAtNode, InvestigateNoise, ReturnToPatrol, FoundPlayer};
+    enum States { PathToNextNode, WaitAtNode, InvestigateNoise, ReturnToPatrol, FoundPlayer };
     States currentstate;
     States laststate = 0;
 
@@ -34,12 +36,12 @@ public class GruntAI : MonoBehaviour {
     //----------------initialization---------------------------------------
 
 
-	void Start ()
+    void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         ChildTransform = this.transform.GetChild(0); //get the body of the grunt
         GameObject levelGeometry = GameObject.FindWithTag("LevelGeometry"); //make sure you only have one object in your scene with this tag!
-        
+
         if (levelGeometry == null)
         { Debug.Log("please apply the LevelGeometry tag to your scenes level prefab"); }
 
@@ -50,30 +52,33 @@ public class GruntAI : MonoBehaviour {
         { Debug.Log(this.name + " has no nodes attached"); }
 
         MaxNode = nodes.Count - 1;
-        
+
         NewState(States.WaitAtNode);
         NextState();
-        
+
     }
 
 
     //----------------------AI states------------------------------------------------------
-    
+
     //----------WaitAtNode
-    
+
     private IEnumerator WaitAtNode()
     {
-        
-        StartCoroutine(RotateGrunt(nodes[CurrentNode].degreeTurn));
-        yield return new WaitForSeconds(nodes[CurrentNode].waitTime + );
+        if (nodes[CurrentNode].degreeTurn != 1)
+        {
+            StartCoroutine(RotateGrunt(nodes[CurrentNode].degreeTurn));
+        }
+
+        yield return new WaitForSeconds(nodes[CurrentNode].waitTime + timeRotating); //wait for specified time plus time it takes to rotate
         StopCoroutine(RotateGrunt(nodes[CurrentNode].degreeTurn));
 
-        if (CurrentNode == MaxNode)
-        { CurrentNode = 0; }
-        else
-        { CurrentNode += 1; }
-        
-        NewState(States.PathToNextNode);    
+        StartCoroutine(RotateGrunt(GetYAngleToward(nodes[IterateToNextNode()].target.position))); //rotate to next target
+        yield return new WaitForSeconds(timeRotating + 0.5f); //wait for rotation to finish
+        StopCoroutine(RotateGrunt(GetYAngleToward(nodes[IterateToNextNode()].target.position)));
+
+        CurrentNode = IterateToNextNode();
+        NewState(States.PathToNextNode);
         NextState();
 
         yield return null;
@@ -83,48 +88,46 @@ public class GruntAI : MonoBehaviour {
 
     private IEnumerator PathToNextNode()
     {
-       
+
         while (currentstate == States.PathToNextNode)
         {
-            
+
             agent.destination = nodes[CurrentNode].target.position;
 
-            if (ChildTransform.position.x == nodes[CurrentNode].target.position.x & ChildTransform.position.z == nodes[CurrentNode].target.position.z) 
+            if (ChildTransform.position.x == nodes[CurrentNode].target.position.x & ChildTransform.position.z == nodes[CurrentNode].target.position.z)
             {
-                
+
                 States nextstate = States.WaitAtNode; NewState(nextstate);
             }
             yield return null;
         }
-        
+
         NextState();
     }
 
 
     //---------------coroutines-----------------------------------------------------
 
-    public IEnumerator RotateGrunt(int targetRotation)
+    public IEnumerator RotateGrunt(float targetRotation)
     {
-        Debug.Log(targetRotation);
-        if()
         float ElapsedTime = 0f;
         float angle;
-        float time = 1f;
-
+        float time = .5f;
+        timeRotating = 0f;
         while (ElapsedTime <= time)
         {
             ElapsedTime += Time.deltaTime;
             float perc = (ElapsedTime / time);
-            angle = Mathf.LerpAngle(this.transform.rotation.y, targetRotation, perc);
+            angle = Mathf.LerpAngle(this.transform.eulerAngles.y, targetRotation, perc);
             transform.eulerAngles = new Vector3(0, angle, 0);
-            Debug.Log(transform.rotation.y);
             yield return new WaitForEndOfFrame();
         }
-        yield return null;  
+        timeRotating = ElapsedTime;
+        yield return null;
     }
 
 
-    
+
 
 
     //---------------state machine functions-----------------------------------------
@@ -147,20 +150,26 @@ public class GruntAI : MonoBehaviour {
     }
 
 
-    /*example state coroutine
 
-    private IEnumerator PathToNextNode()
+    //-------------------functions---------------------------
+
+    public float GetYAngleToward(Vector3 target)
     {
-        while (currentstate == States.PathToNextNode)
-        {
-
-            if (terminate)
-            {
-                States nextstate = States.WaitAtNode;  NewState (nextstate);
-            }
-            yield return null;
-        }
-        NextState ();
+        var trans = this.transform.position;
+        Vector3 direction = (target - trans).normalized;
+        Quaternion q = Quaternion.LookRotation(direction);
+        Vector3 angle = q.eulerAngles;
+        return angle.y;
     }
-    */
+
+    public int IterateToNextNode() //returns next Node in nodes
+    {
+        int returnVale;
+        returnVale = (CurrentNode == MaxNode) ? 0 : returnVale = CurrentNode + 1;
+        return returnVale;
+    }
+
+
+
 }
+
