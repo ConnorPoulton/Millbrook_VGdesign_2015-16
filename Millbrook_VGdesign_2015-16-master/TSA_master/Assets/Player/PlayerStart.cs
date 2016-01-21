b54822b3cd35e0cd04eeabf5e873b39b27720c7e
ConnorPoulton <poulton.cw@gmail.com>
2016-01-20 23:05:26 -0500

    Started Camera work
    added Check cam in bounds functions

---
+++ Millbrook_VGdesign_2015-16-master/TSA_master/Assets/Player/PlayerStart.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TSA;
+
+public class PlayerStart : MonoBehaviour {
+
+    Vector3 CameraStartPosition;
+    Vector3 CameraTargetPosition;
+    Quaternion CameraStartRotation;
+    Quaternion CameraTargetRotation;
+
+    public Transform player;
+    public RectTransform StartCanvas;
+    public RectTransform GameOverCanvas;
+    RectTransform canvas; //store refrence to instantiated canvas
+    public float letterPause;
+    public float TimeToLerp;
+    Text title;
+    Text paragraph;
+
+    public int CameraStartZoom;
+    public string LevelName;
+    public string LevelDescription;
+
+    void Start()
+    {
+        Vector3 trans = this.transform.position;
+        CameraStartPosition = Camera.main.transform.position;
+        CameraStartRotation = Camera.main.transform.rotation;
+        CameraTargetPosition = new Vector3(trans.x, trans.y + CameraStartZoom, trans.z);
+        CameraTargetRotation = Quaternion.Euler(new Vector3(90,0,0));    
+        canvas = Instantiate(StartCanvas, StartCanvas.position, StartCanvas.rotation) as RectTransform;
+        title = canvas.GetChild(0).GetComponent<Text>();
+        title.text = LevelName;
+        paragraph = canvas.GetChild(1).GetComponent<Text>();
+        paragraph.text = "";
+        //subscribe to events
+        ResourceManager.playerFound += GameOver;
+        ResourceManager.playerClearedLevel += LevelClear;
+
+        StartCoroutine(TextType()); //text type triggers LerpCamera, LerpCamera spawns player when complete
+    }
+
+    void OnDisable()
+    {
+        ResourceManager.playerFound -= GameOver;
+        ResourceManager.playerClearedLevel -= LevelClear;         
+    }
+
+    public void GameOver()
+    {
+        Instantiate(GameOverCanvas, GameOverCanvas.position, GameOverCanvas.rotation);
+        StartCoroutine(WaitForGameOver());
+    }
+
+    public void LevelClear()
+    {
+
+    }
+
+    public IEnumerator TextType()
+    {
+        foreach (char letter in LevelDescription.ToCharArray())
+        {
+            paragraph.text += letter;
+            if (Input.GetButton("Jump") == true)
+            {
+
+                paragraph.text = LevelDescription;
+                break;
+            }
+            yield return new WaitForSeconds(letterPause);
+        }
+
+        yield return new WaitForSeconds(.3f);
+
+        while (!Input.GetButton("Jump"))
+            yield return null;
+
+        canvas.gameObject.SetActive(false);
+        StartCoroutine(LerpCamera()); 
+        yield break;
+    }
+
+
+
+    public IEnumerator LerpCamera()
+    {
+        float ElapsedTime = 0f;
+
+        while (ElapsedTime <= TimeToLerp)
+        {
+            ElapsedTime += Time.deltaTime;
+            float perc = (ElapsedTime / TimeToLerp);
+            Camera.main.transform.position = Vector3.Lerp(CameraStartPosition, CameraTargetPosition, perc);
+            Camera.main.transform.rotation = Quaternion.Lerp(CameraStartRotation, CameraTargetRotation, perc);
+            yield return new WaitForEndOfFrame();
+        }
+
+    yield return new WaitForSeconds(1);
+    Instantiate(player, this.transform.position, this.transform.rotation);
+    yield break;
+    }
+
+
+    public IEnumerator WaitForGameOver()
+    {
+        while (true)
+        {
+            if (Input.GetButtonDown("pause"))
+                Application.LoadLevel(Application.loadedLevel);
+            yield return null;
+        }        
+    }
+}
---
+++ Millbrook_VGdesign_2015-16-master/TSA_master/Assets/Resources/ResourceManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TSA
+{
+    static public class ResourceManager
+    {
+        //event delegates
+        public delegate void PlayerEventHandler();
+
+        static public event PlayerEventHandler playerFound;
+        static public event PlayerEventHandler playerClearedLevel;
+
+        //constant values
+        public static readonly Vector3 OUTOFBOUNDS = new Vector3(0, -999, 0);
+
+        //scene values
+        public static Vector3 SceneLLBounds;
+        public static Vector3 SceneURBounds;
+        
+
+        public static void CallplayerFound()
+        {
+            if (playerFound != null)
+                playerFound();
+        }
+
+        public static void CallplayerClearedLevel()
+        {
+            if (playerClearedLevel != null)
+                playerClearedLevel();
+        }
+
+        public static Vector3 MakeCameraVecInBounds(Vector3 target)
+        {
+            float CamXOffSet = 0;
+            float CamZOffSet = 0;
+
+            Vector3 CamLLBound = Camera.main.ViewportToWorldPoint(new Vector3(0, target.y, 0));
+            Vector3 CamURBound = Camera.main.ViewportToWorldPoint(new Vector3(1, target.y, 1));
+
+            CamXOffSet = Mathf.Abs(CamLLBound.x / 2); //gets distance in world space from edge of camera view to camera origin
+            CamZOffSet = Mathf.Abs(CamLLBound.z / 2);
+
+            float ZMin = ResourceManager.SceneLLBounds.z + 8;
+            float ZMax = ResourceManager.SceneURBounds.z - 8;
+            float XMax = ResourceManager.SceneURBounds.x - 8;
+            float XMin = ResourceManager.SceneLLBounds.x - 8;
+
+            target.x = (target.x < XMin) ? XMin : (target.x > XMax) ? XMax : target.x;
+            target.z = (target.z < ZMin) ? ZMin : (target.z > ZMax) ? ZMax : target.z;
+
+            return target;
+        }
+    }
+}
